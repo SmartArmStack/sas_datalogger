23343abbb1984fbcbc9987b083cf7ed472be00d8
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2021-07-06 21:31:07 -0700

    [Python] Added test script, interface seems to be working fine.

--- scripts/rosilo_datalogger_interface_test.py
+++ scripts/rosilo_datalogger_interface_test.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 import rospy
+import time
+import numpy as np
 from rosilo_datalogger import DataloggerInterface
 
+rospy.init_node('rosilo_datalogger_interface_test', disable_signals=True)
+
 datalogger = DataloggerInterface(20)
+while not rospy.is_shutdown():
+    datalogger.log("a_str", "str")
+    datalogger.log("a_float", 2.0)
+    datalogger.log("an_int", 2)
+    datalogger.log("an_int_array", np.array([2, 2, 2, 2]))
+    time.sleep(1)
--- src/rosilo_datalogger/datalogger_interface.py
+++ src/rosilo_datalogger/datalogger_interface.py
@@ -30,7 +30,7 @@ from std_msgs.msg import Float64MultiArray
 class DataloggerInterface:
     def __init__(self, queue_size):
         # sc_save = node_handle.serviceClient < rosilo_datalogger::Save > ("/rosilo_datalogger/save");
-        self.publisher_add_value_ = rospy.Publisher("set/desired_pose",
+        self.publisher_add_value_ = rospy.Publisher("/rosilo_datalogger/addvaluemsg",
                                                     AddValueMsg,
                                                     queue_size=queue_size)
 
@@ -38,24 +38,34 @@ class DataloggerInterface:
         msg = AddValueMsg()
         msg.name = name
         if type(value) is str:
-            strvalue = value
+            msg.strvalue = value
+            self.publisher_add_value_.publish(msg)
+            return
+
         elif type(value) is float or type(value) is int:
-            msg.value = Float64MultiArray(value)
+            msg.value = [value]
+            self.publisher_add_value_.publish(msg)
+            return
+
         else:
             # If the array is (x, 1)
             # If the array is (1, y)
             try:
                 (x, y) = value.shape
                 msg.value = value.reshape(max(x, y))
+                self.publisher_add_value_.publish(msg)
+                return
+
             except ValueError as e:
                 pass
             # If the array is (x,)
             try:
                 (x,) = value.shape
                 msg.value = value
+                self.publisher_add_value_.publish(msg)
+                return
+
             except ValueError as e:
                 pass
 
-            raise Exception("DataloggerInterface::Unknown value type={}.".format(type(value)))
-
-        self.publisher_add_value_.publish(msg)
+        raise Exception("DataloggerInterface::Unknown value type={}.".format(type(value)))
