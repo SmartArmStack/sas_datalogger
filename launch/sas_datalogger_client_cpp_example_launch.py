015e176d4425ca3fad078381149c8a0647fb90e6
Frederico Afonso <frederico.fernandes.afonso@gmail.com>
2026-05-08 16:31:31 +0100

    Edit 'sas_datalogger_client_cpp_example_launch.py' to not need the new 'sas_datalogger_client_cpp_docker_example_launch.py'

--- launch/sas_datalogger_client_cpp_example_launch.py
+++ launch/sas_datalogger_client_cpp_example_launch.py
@@ -1,15 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# Run the example "src/examples/sas_datalogger_client_example.cpp"
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            output='screen',
-            emulate_tty=True,
-            package='sas_datalogger',
-            executable='sas_datalogger_node.py',
-            name='sas_datalogger'
-        ),
         Node(
             output='screen',
             emulate_tty=True,
