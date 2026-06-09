db8e0cd923d2a7bca7af7d52cd66916c27c3aa68
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2026-06-09 15:43:47 +0100

    [install.sh] Removing unnecessary dependency.

--- docker/install.sh
+++ docker/install.sh
@@ -2,6 +2,6 @@
 set -e
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y libxcb-cursor-dev
-python3 -m pip install bota_driver PyQt6 pyqtgraph --break-system-packages
+python3 -m pip install PyQt6 pyqtgraph --break-system-packages
 # https://github.com/bhowiebkr/laser-level-webcam/issues/28
 python3 -m pip install pyqtdarktheme==2.1.0 --ignore-requires-python --break-system-packages
